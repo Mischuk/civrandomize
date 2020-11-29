f6fdcc243d59b50249f561fde6c404082e8c0bff
Alexander Mischuk <mischuk.alexander@gmail.com>
2020-11-29 20:33:23 +0200

    add static

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 const { MONGO_URL, PORT } = require("./core/constants");
 
 const app = express();
 app.use(express.json({ extended: true}));
 
 app.use("/api/auth", require("./routes/auth.routes"));
+
+if ( process.env.NODE_ENV === "production" ) {
+    app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    });
+}
 
 async function start() {
     try {
